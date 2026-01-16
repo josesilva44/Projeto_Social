2c11c35f987354495098beab41219a22fdd79a19
josesilva44 <jose.silva.44@academico.ifpb.edu.br>
2026-01-16 19:09:30 +0000

    Docker Versão Corrigida

--- Escola_pj/Escola/Escola/settings.py
+++ Escola_pj/Escola/Escola/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -113,10 +114,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 
-STATICFILES_DIRS = [BASE_DIR / "base" / "static"] 
+# STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# STATICFILES_DIRS = [BASE_DIR / "base" / "static"] 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "testserver"]  
 
 
